b396daf087a26687787d14287acc4e2342367507
carolbaam <carolinabaam@gmail.com>
2018-01-22 19:45:03 -0600

    modales y estilos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 function loadPage() {
-  $(".modal").modal();
+  
   
   $("#filtro").keydown(filterOptions);
 }
@@ -7,31 +7,32 @@ function loadPage() {
 
 function paintRestaurantsHtml (restaurant) {
   // crear elementos con DOM
-  var $restaurant = $("<article />", {
+  var $div = $("<div />", {
     "class": "panel panel-default"
   });
   var $containerRestaurantName = $("<h4 />");
   var $containerType = $("<p />");
   var  $containerPrice = $("<p />");
-  
+  //incluir info a modal
+
 
   // Agregamos atributos y eventos a los elementos creados en el dom
-  
-
+  $div.attr('data-toggle','modal')
+  $div.attr('data-target','.bs-example-modal-sm')
   // Asignando valores
-
+  
   $containerType.text(restaurant.type);
   $containerRestaurantName.text(restaurant.name);
   $containerPrice.text(restaurant.price);
 
-  $restaurant.append($containerRestaurantName);
-  $restaurant.append($containerType);
-  $restaurant.append($containerPrice);
+  $div.append($containerRestaurantName);
+  $div.append($containerType);
+  $div.append($containerPrice);
   // console.log($newContact);
   // agregamos lo que creamos con el Dom a un elemento existente del html
 
 
-  $("#section-food").prepend($restaurant);
+  $("#section-food").prepend($div);
   
 }
 
@@ -63,49 +64,9 @@ function filterOptions (){
   
 }
 
-/*
-function filterContacts (){
-  var searchContact = $("#searcher").val().toLowerCase();
-    if($("#searcher").val().trim().length > 0) {
-        var filteredContacts = contacts.filter(function(contact) {
-           // console.log(contact);
-            return contact.name.toLowerCase().indexOf(searchContact) >= 0;
-        });
-      $("#publish-contacts").empty();
-      filteredContacts.forEach(function(contact){
-        paintContactsInHtml(contact);
-      });
-    } else {
-      $("#publish-contacts").empty();
-      contacts.forEach(function(contact){
-        paintContactsInHtml(contact);
-      });      
-    }
 
-  
-  // console.log(filteredContacts);
 
-  
-}*/
 
-var data = [
-	{"name": "Casa Juan", "type": "mexicana", "price": "$$$"},
-	{"name": "Taquitos bonitos", "type": "mexicana", "price": "$"},
-	{"name": "Latoso", "type": "mexicana", "price": "$"},
-	{"name": "Chalupas Frescas", "type": "mexicana", "price": "$$$"},
-	{"name": "Caminito", "type": "mexicana", "price": "$$"},
-	{"name": "Italianissimo", "type": "italiana", "price": "$"},
-	{"name": "Jemima", "type": "italiana", "price": "$$$"},
-	{"name": "Buffos", "type": "italiana", "price": "$$"},
-	{"name": "Linguine", "type": "italiana", "price": "$"},
-	{"name": "La Vitta", "type": "italiana", "price": "$$$"},
-	{"name": "Katzu", "type": "japones", "price": "$$"},
-	{"name": "Shitzu", "type": "japones", "price": "$"},
-	{"name": "Ramens", "type": "japones", "price": "$$$"},
-	{"name": "Sushittito", "type": "japones", "price": "$$"},
-	{"name": "kutimanzuto", "type": "japones", "price": "$"},
-	
-];
 
   
 $(document).ready(loadPage);
